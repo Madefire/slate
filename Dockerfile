607325d9d1c8d95094e8bd9fed590d1aaea9a255
Max Mautner <max.mautner@gmail.com>
2016-12-05 18:34:13 -0800

    upload to s3

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM ruby:onbuild
 MAINTAINER Adrian Perez <adrian@adrianperez.org>
-VOLUME /usr/src/app/source
-EXPOSE 4567
 
 RUN apt-get update && apt-get install -y nodejs \
 && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+VOLUME /usr/src/app
+EXPOSE 4567
 
 CMD ["bundle", "exec", "middleman", "server"]
--- deploy.sh
+++ deploy.sh
@@ -14,4 +14,4 @@ docker run --rm \
     -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     -v $PWD:/usr/src/app \
-    -w /usr/src/app $DOCKER_TAG /bin/bash -c "bundle exec middleman $BUILD_DIR --clean; s3cp --recursive $BUILD_DIR/ s3://$S3_BUCKET/"
+    -w /usr/src/app $DOCKER_TAG /bin/bash deploy2.sh
